369a877400d6f4d9aa7bd1b4145290dac5d15e68
Prashant <prashantnkolase@gmail.com>
2021-08-07 22:07:34 +0530

    Few Changes have made

--- index.java
+++ index.java
@@ -2,6 +2,11 @@ public class index{
 	public static void main(String[] args){
 	System.out.println("Hello World");
 	
+	
 	}
+	public void m1(){
+	System.out.println("InSide Method 1")	
+	}
+	
 	
 }
